50457c0ff5d0180f0e7a0d1212896b5e34be135d
Jordan Manthey <jordanmanthey@Jordans-MacBook-Pro.local>
2022-02-12 02:44:52 -0800

    initial commit

---
+++ src/com/pipelines/DatabaseManager.java
@@ -0,0 +1,4 @@
+package com.pipelines;
+
+public class DatabaseManager {
+}
---
+++ src/com/pipelines/Main.java
@@ -0,0 +1,8 @@
+package com.pipelines;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
