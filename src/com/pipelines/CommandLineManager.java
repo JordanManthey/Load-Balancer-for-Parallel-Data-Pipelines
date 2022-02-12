b84996ee2014462fbc29db77ca33e434d403e48c
Jordan Manthey <jordanmanthey@Jordans-MacBook-Pro.local>
2022-02-12 03:03:22 -0800

    created CommandLineManager class

--- src/com/pipelines/CommandLineManager.java
+++ src/com/pipelines/CommandLineManager.java
@@ -1,4 +1,14 @@
 package com.pipelines;
 
 public class CommandLineManager {
+
+    public static String JDBC_DRIVER;
+    public static String DB_URL;
+    public static String USER;
+    public static String PASS;
+    public static String NUM_APPS;
+    public static String NUM_CORES;
+    public static String EXCLUDE;
+    public static String SOURCE_TYPE;
+    
 }
